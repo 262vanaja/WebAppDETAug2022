60b159764024fbee0e090b7b2c9c5244ab6fdf05
etg1 <etg1@BG4WS1532>
2022-09-08 18:43:28 +0530

    9/8/22

---
+++ MVCDemo/Controllers/Events.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Diagnostics;
+using System.Xml;
+using MVCDemo.Models;
+
+namespace MVCDemo.Controllers
+{
+    public class EventsController1 : Controller
+    {
+
+        public List<Booking> events { get; set; }
+
+        public IActionResult Index()
+        {
+            events = new List<Booking>();
+            events.Add(new Booking { Title = " ask CEO", EventDate = DateTime.Today, Qty = 10 });
+            events.Add(new Booking { Title = " ask Culture ", EventDate = DateTime.Today, Qty = 10 });
+            events.Add(new Booking { Title = " ask sports", EventDate = DateTime.Today, Qty = 10 });
+            events.Add(new Booking { Title = " ask Indoor", EventDate = DateTime.Today, Qty = 10 });
+
+            return View(events);
+        }
+        
+    }
+}
---
+++ MVCDemo/Controllers/PizzaController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCDemo.Models;
+using MVCDemo.Service;
+using System.Security.Cryptography.X509Certificates;
+
+namespace MVCDemo.Controllers
+{
+    public class PizzaController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            List<Pizza> pizzas = PizzaService.GetAll();
+            return View(pizzas);
+
+        }
+        public IActionResult Detail(int id)
+        {
+            Pizza p = PizzaService.Get(id);
+            return View(p);
+        }
+        public IActionResult List(int id)
+        {
+            List<Pizza> pizzas = PizzaService.GetAll();
+            return View(pizzas);
+        }
+        public IActionResult Create()
+
+        {
+            return View();
+
+        }
+        [HttpPost]
+
+        public IActionResult Create(Pizza p)
+        {
+            PizzaService.Add(p);
+            return RedirectToAction("List");
+        }
+
+
+
+
+
+        public IActionResult Delete( int Id)
+
+
+        {
+            Pizza p = PizzaService.Get(Id);
+              if (p != null)
+
+            return View(p);
+
+            return RedirectToAction("List");
+        }
+        [HttpPost]
+
+        public IActionResult Delete(Pizza p)
+        {
+            PizzaService.Delete(p.Id);
+            return RedirectToAction("List");
+        }
+    }
+}
+
+    
+
+        
+        
+    
