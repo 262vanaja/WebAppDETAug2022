5b020fa9ddb6bd68ac86d02cbf74a94f08e10064
etg1 <etg1@BG4WS1532>
2022-09-12 09:15:08 +0530

    7/9/2022

--- MVCDemo/Controllers/PizzaController.cs
+++ MVCDemo/Controllers/PizzaController.cs
@@ -8,63 +8,65 @@ namespace MVCDemo.Controllers
     public class PizzaController : Controller
     {
 
-        public IActionResult Index()
-        {
-            List<Pizza> pizzas = PizzaService.GetAll();
-            return View(pizzas);
+    
+            public IActionResult Index()
+            {
+                List<Pizza> pizzas = PizzaService.GetAll();
+                return View(pizzas);
+            }
 
-        }
-        public IActionResult Detail(int id)
-        {
-            Pizza p = PizzaService.Get(id);
-            return View(p);
-        }
-        public IActionResult List(int id)
-        {
-            List<Pizza> pizzas = PizzaService.GetAll();
-            return View(pizzas);
-        }
-        public IActionResult Create()
+            public IActionResult Details(int id)
+            {
+                Pizza p = PizzaService.Get(id);
+                return View(p);
+            }
 
-        {
-            return View();
+            public IActionResult List()
+            {
+                //TempData["FestivalOffer"] = 25;
+                List<Pizza> pizzas = PizzaService.GetAll();
+                return View(pizzas);
 
-        }
-        [HttpPost]
+            }
 
-        public IActionResult Create(Pizza p)
-        {
-            PizzaService.Add(p);
-            return RedirectToAction("List");
-        }
+            public IActionResult Create()
+            {
+                return View();
+            }
+            [HttpPost]
+            public IActionResult Create(int id, string name, int size, decimal price, bool isglutenfree)
+            {
+                Pizza p = new Pizza { Id = id, Name = name, Size = (PizzaSize)size, Price = price, IsGlutenFree = isglutenfree };
+                PizzaService.Add(p);
+                return RedirectToAction("List");
+            }
+            public IActionResult Delete(int id)
+            {
+                Pizza p = PizzaService.Get(id);
+                if (p != null)
+                    return View(p);
+                else
+                    return RedirectToAction("List");
+            }
 
+            [HttpPost]
+            public IActionResult Delete(Pizza p)
+            {
+                PizzaService.Delete(p.Id);
+                return RedirectToAction("Delete");
+            }
+            public IActionResult Edit()
+            {
+                return View();
+            }
 
-
-
-
-        public IActionResult Delete( int Id)
-
-
-        {
-            Pizza p = PizzaService.Get(Id);
-              if (p != null)
-
-            return View(p);
-
-            return RedirectToAction("List");
-        }
-        [HttpPost]
-
-        public IActionResult Delete(Pizza p)
-        {
-            PizzaService.Delete(p.Id);
-            return RedirectToAction("List");
-        }
+            [HttpPost]
+            public IActionResult Edit(int id, string name, int size, decimal price, bool isglutenfree)
+            {
+                Pizza p = new Pizza { Id = id, Name = name, Size = (PizzaSize)size, Price = price, IsGlutenFree = isglutenfree };
+                PizzaService.Update(p);
+                return RedirectToAction("List");
+            }
+        
     }
 }
-
-    
-
-        
-        
-    
